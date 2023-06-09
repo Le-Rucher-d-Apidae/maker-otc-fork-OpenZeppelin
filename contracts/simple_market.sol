b70539de565eb27052018fe2ce272e1926e16e9b
BenoistP <bp_github.com@gazouille.net>
2023-06-09 21:15:07 +0200

    errors

--- contracts/simple_market.sol
+++ contracts/simple_market.sol
@@ -85,8 +85,44 @@ contract EventfulMarket {
 
 }
 
+contract ErrorCodes {
+    // S Series = Security/Authorization
+    string internal constant _S000 = "S000_REENTRANCY_ATTEMPT";
+
+    // T Series = Trades/Offers
+    string internal constant _T001 = "T001_BUY_TOKEN_NOT_ALLOWED";
+    string internal constant _T002 = "T002_SELL_TOKEN_NOT_ALLOWED";
+    // string internal constant _T003 = "T003_WRONG_TRADING_PAIR";
+
+/*
+    string internal constant _S101 = "S101_NOT_AUTHORIZED";
+    // F Series = Funds
+
+    string internal constant _F101 = "F101_BALANCE_NOT_ENOUGH";
+    string internal constant _F102 = "F102_ADDRESS_CANT_BE_0";
+    string internal constant _F103 = "F103_TOKEN_NOT_ALLOWED";
+    string internal constant _F104 = "F104_TRANSFER_FAILED";
+
+    // T Series = Trades/Offers
+
+    string internal constant _T101 = "T101_OFFER_NOT_PRESENT";
+    string internal constant _T102 = "T102_OFFER_ID_NOT_VALID";
+    string internal constant _T103 = "T103_OFFER_TYPE_NOT_VALID";
+    string internal constant _T104 = "T104_OFFER_AMOUNT_LOW";
+    string internal constant _T105 = "T105_OFFER_AMOUNT_HIGH";
+    string internal constant _T106 = "T106_OFFER_AMOUNT_NOT_VALID";
+    string internal constant _T107 = "T107_TOKENS_CANT_BE_THE_SAME";
+    string internal constant _T108 = "T108_NOT_ENOUGH_OFFERS_PRESENT";
+    string internal constant _T109 = "T109_BUY_FAILED";
+    string internal constant _T110 = "T110_UNSORT_FAILED";
+    string internal constant _T111 = "T111_FILL_AMOUNT_LOW";
+    string internal constant _T112 = "T112_FILL_AMOUNT_HIGH";
+*/
+
+}
+
 //contract SimpleMarket is EventfulMarket, DSMath {
-contract SimpleMarket is EventfulMarket, Ownable {
+contract SimpleMarket is EventfulMarket, ErrorCodes, Ownable {
 
     //using Address for address;
     address NULL_ADDRESS = address(0x0);
@@ -99,6 +135,15 @@ contract SimpleMarket is EventfulMarket, Ownable {
 
     ERC20 mainTradableToken; // ApidaeToken
     mapping (ERC20=>bool) tradableTokens; // mainTradableToken must not be in this list
+
+    // error Locked();
+    // error Unauthorized();
+
+    /// Invalid Trading pair
+    /// @param buyToken token to buy.
+    /// @param sellToken token to sell.
+    error InvalidTradingPair(ERC20 buyToken, ERC20 sellToken);
+    error TestError();
 
     constructor(ERC20 _mainTradableToken) {
         mainTradableToken = _mainTradableToken;
@@ -133,7 +178,8 @@ contract SimpleMarket is EventfulMarket, Ownable {
     }
 
     modifier synchronized {
-        require(!locked, "Reentrancy attempt");
+        // require(!locked, "Reentrancy attempt");
+        require(!locked, _S000);
         locked = true;
         _;
         locked = false;
@@ -166,19 +212,32 @@ contract SimpleMarket is EventfulMarket, Ownable {
         //  - mainTradableToken / tradableTokens[_buy_gem])
         //  - tradableTokens[_pay_gem]) / mainTradableToken
 
+        /*
         // Sell mainTradableToken
         _pay_gem==mainTradableToken
         ?
         // Buy token must be tradable
-        require(tradableTokens[_buy_gem], "buy token not allowed")
+        // require(tradableTokens[_buy_gem], "buy token not allowed")
+        require(tradableTokens[_buy_gem], _T001)
         :
         // Buy mainTradableToken
         _buy_gem==mainTradableToken
             ?
             // Sold token must be tradable
-            require( tradableTokens[_pay_gem], "sell token not allowed" ):
+            // require( tradableTokens[_pay_gem], "sell token not allowed" ):
+            require( tradableTokens[_pay_gem], _T002 ):
             // mainTradableToken is neither sold or bought : revert
-            revert("wrong trading pair") ;
+            // revert("wrong trading pair") ;
+            revert(_T003) ;
+            // revert InvalidTradingPair(_pay_gem, _buy_gem);
+        */
+        if (_pay_gem==mainTradableToken) {
+            require(tradableTokens[_buy_gem], _T001);
+        } else if (_buy_gem==mainTradableToken) {
+            require(tradableTokens[_pay_gem], _T002);
+        } else {
+            revert InvalidTradingPair(_pay_gem, _buy_gem);
+        }
         _;
     }
 
