601f9ed9a547d63a1fbd41f3e1c25fb271b64246
BenoistP <bp_github.com@gazouille.net>
2023-06-11 19:32:53 +0200

    Update restricted_suspendable_simple_market.sol

--- contracts/restricted_suspendable_simple_market.sol
+++ contracts/restricted_suspendable_simple_market.sol
@@ -86,7 +86,7 @@ contract RestrictedSuspendableSimpleMarket is SuspendableSimpleMarket, Restricte
     modifier checkOfferTokens(ERC20 _pay_gem, ERC20 _buy_gem) override {
         // Since tradable tokens are whitelisted, no need to check for address(0x0)
         // Check for token : one must be mainTradableToken, other must be tradable
-        console2.log( "checkOfferTokens RestrictedSuspendableSimpleMarket" );
+        console2.log( "modifier checkOfferTokens:RestrictedSuspendableSimpleMarket" );
 
         // Sell mainTradableToken
         if (_pay_gem==mainTradableToken) {
