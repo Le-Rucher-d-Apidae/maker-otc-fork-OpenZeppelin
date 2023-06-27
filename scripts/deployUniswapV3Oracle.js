4a1e1ffdb3be9939bfa702c9a1970fc7396d13bf
BenoistP <bp_github.com@gazouille.net>
2023-06-27 19:21:34 +0200

    fee must be hex

--- scripts/deployUniswapV3Oracle.js
+++ scripts/deployUniswapV3Oracle.js
@@ -13,7 +13,15 @@ const contractName = "UniswapV3Twap"
 const UniswapV3Factory_addr ="0x1F98431c8aD98523631AE4a59f267346ea31F984"; // UniswapV3Factory Address on Mainnet, Goerli, Arbitrum, Optimism, Polygon Address
 const wEth = "0xA6FA4fB5f76172d178d61B04b0ecd319C5d1C0aa" // wEth Address on Polygon Mumbai Testnet
 const wMatic = "0x9c3C9283D3e44854697Cd22D3Faa240Cfb032889" // wMatic Address on Polygon Mumbai Testnet
-const fee = 3000 // 0.3% Fee
+
+const FEE_0_30 = 3000 // 0x0bb8 0.30% Fee
+const FEE_1_00 = 10000 // 0x2710 1% Fee
+const FEE_0_05 = 500 // 0x01f4 0.05% Fee
+
+
+
+const FEE =  '0x' + FEE_0_30.toString(16) // convert to hex
+console.log( `FEE = ${FEE}` );
 
 const args = [
   // address _factory,
@@ -23,7 +31,7 @@ const args = [
   // address _token1,
   wMatic,
   // uint24 _fee
-  fee
+  FEE
 ];
 
 
@@ -70,7 +78,7 @@ async function main() {
     UniswapV3Factory_addr,
     wEth,
     wMatic,
-    fee,
+    FEE,
     {
     // from: deployer,
     // log: true,
