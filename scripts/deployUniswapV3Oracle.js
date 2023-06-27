6d87182584e515ccabfa028289c8b62194c6023e
BenoistP <bp_github.com@gazouille.net>
2023-06-27 20:44:53 +0200

    Oracle deployment

--- scripts/deployUniswapV3Oracle.js
+++ scripts/deployUniswapV3Oracle.js
@@ -6,37 +6,10 @@
 // When running the script with `npx hardhat run <script>` you'll find the Hardhat
 // Runtime Environment's members available in the global scope.
 const hre = require("hardhat");
+const args = require("./arguments/deployUniswapV3Oracle-arguments")
+// const fs = require('fs');
+const fs = require('fs/promises');
 
-// const args = require("./arguments/deployUniswapV3Oracle-arguments")
-
-const contractName = "UniswapV3Twap"
-const UniswapV3Factory_addr ="0x1F98431c8aD98523631AE4a59f267346ea31F984"; // UniswapV3Factory Address on Mainnet, Goerli, Arbitrum, Optimism, Polygon Address
-const wEth = "0xA6FA4fB5f76172d178d61B04b0ecd319C5d1C0aa" // wEth Address on Polygon Mumbai Testnet
-const wMatic = "0x9c3C9283D3e44854697Cd22D3Faa240Cfb032889" // wMatic Address on Polygon Mumbai Testnet
-
-const FEE_0_30 = 3000 // 0x0bb8 0.30% Fee
-const FEE_1_00 = 10000 // 0x2710 1% Fee
-const FEE_0_05 = 500 // 0x01f4 0.05% Fee
-
-
-
-const FEE =  '0x' + FEE_0_30.toString(16) // convert to hex
-console.log( `FEE = ${FEE}` );
-
-const args = [
-  // address _factory,
-  UniswapV3Factory_addr,
-  // address _token0,
-  wEth,
-  // address _token1,
-  wMatic,
-  // uint24 _fee
-  FEE
-];
-
-
-
-// const DEFAULT_ARG_MESSAGE = "Hello, Hardhat! (default)"
 const listArgs = function (obj)
 {
   console.log( `---------` );
@@ -45,7 +18,6 @@ const listArgs = function (obj)
     { console.log( `args[ '${key}' ]= '${obj[key]}'` ); }
     console.log( `---------` );
 } // listArgs
-
 
 async function main() {
   // Hardhat always runs the compile task when running scripts with its command
@@ -58,27 +30,24 @@ async function main() {
   listArgs( args );
   // ethers is available in the global scope
   const [deployer] = await ethers.getSigners();
-  console.dir( deployer)
+  // console.dir( deployer)
   console.log(
-    `Deploying the contract ${contractName} with the account:`,
+    `Deploying the contract ${args["contractName"]} with the account:`,
     await deployer.getAddress()
   );
+
+  const { getChainId } = hre
+  const chainId = await getChainId()
+  console.log( `chainId = ${chainId}` );
+
   // We get the contract to deploy
-  const UniswapV3Twap_CF = await hre.ethers.getContractFactory(contractName);
-
-  
-  // console.log( `deployUniswapV3Oracle.js: args["message"] = ${args["message"]}` );
-  // const message = args["message"] ? args["message"] : DEFAULT_ARG_MESSAGE
-  // console.log( `deployUniswapV3Oracle.js: args: message = ${message}` );
-
-  // const greeter = await UniswapV3Twap_CF.deploy( /*args*/ message );
+  const UniswapV3Twap_CF = await hre.ethers.getContractFactory(args["contractName"]);
 
   const UniswapV3Twap = await UniswapV3Twap_CF.deploy(
-    // message,
-    UniswapV3Factory_addr,
-    wEth,
-    wMatic,
-    FEE,
+    args[ "factory" ],
+    args[ "token0" ],
+    args[ "token1" ],
+    args[ "fee" ],
     {
     // from: deployer,
     // log: true,
@@ -86,10 +55,23 @@ async function main() {
     }
   );
 
+  console.debug(typeof(args));
+  const contractArgs = { ...args }
+  delete contractArgs.contractName
+  jsonArgs = JSON.stringify(contractArgs)
+
+  // const d = new Date();
+  let dateText = new Date().toLocaleString();
+
+
   await UniswapV3Twap.deployed();
 
-  console.log(`${contractName} deployed to:`, UniswapV3Twap.address);
-}
+  const deploymentResult = `${dateText}: ${args["contractName"]} deployed to: ${UniswapV3Twap.address} with args: ${jsonArgs} on chain ${chainId}\n`
+
+  console.info( deploymentResult );
+
+  await fs.appendFile(`./scripts/deployments/deployments-${args["contractName"]}.txt`, deploymentResult);
+} // main
 
 // We recommend this pattern to be able to use async/await everywhere
 // and properly handle errors.
