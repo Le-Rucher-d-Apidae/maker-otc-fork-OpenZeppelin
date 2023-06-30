5d6da1b31564e803dc156a4465176a4f24f0abba
BenoistP <bp_github.com@gazouille.net>
2023-06-30 19:44:12 +0200

    Matching Market deployment

--- hardhat-deploy/deploy-params/RestrictedSuspendableMatchingMarket-params.js
+++ hardhat-deploy/deploy-params/RestrictedSuspendableMatchingMarket-params.js
@@ -2,10 +2,26 @@
 const contractName = "RestrictedSuspendableMatchingMarket"
 
 
+const isMarketSuspended = false;
+// const dustToken = "0x0000000";
+
+ONE_USDC = 1_000_000; // USDC: 6 decimals
+ONE_USDT = ONE_USDC; // USDT: 6 decimals
+ONE_BUSD = 1_000_000_000_000_000_000; // BUSD: 18 decimals
+ONE_DAI = ONE_BUSD; // DAI: 18 decimals
+
+// Minimum trade amount & dust limit
+const dustLimit = ONE_USDC / 10; // 0.1 USDC
+// const dustLimit = ONE_BUSD / 10; // 0.1 BUSD
+
 let params ={
   contractName: contractName,
-  args: [],
-  log: true,
+  args: {
+    31337:  [isMarketSuspended, /* dustToken, */ dustLimit], // HardHat : use Polygon Mumbai Testnet or Polygon Mainnet
+    80001:  [isMarketSuspended, /* dustToken, */ dustLimit], // Polygon Mumbai Testnet
+    137:    [isMarketSuspended, /* dustToken, */ dustLimit], // Polygon Mainnet
+    },
+    log: true,
 }
 
 module.exports = params;
--- hardhat-deploy/deploy/deployMatchingMarket.js
+++ hardhat-deploy/deploy/deployMatchingMarket.js
@@ -1,8 +1,14 @@
-const params = require("../deploy-params/RestrictedSuspendableMatchingMarket-params")
+const matchingMarket_params = require("../deploy-params/RestrictedSuspendableMatchingMarket-params")
 
-const contractName = params.contractName;
-const log = params.log;
-const args = params.args;
+const token_params = require("../deploy-params/ApidaeToken-params")
+const token_contractName = token_params.contractName;
+
+
+const uniswapV3Oracle_params = require("../deploy-params/UniswapV3Oracle-params")
+const uniswapV3Oracle_contractName = uniswapV3Oracle_params.contractName;
+
+const contractName = matchingMarket_params.contractName;
+const log = matchingMarket_params.log;
 
 module.exports = async (
   {
@@ -12,17 +18,40 @@ module.exports = async (
   getUnnamedAccounts,
 }) => {
   const {deploy, getNetworkName} = deployments;
-  getChainId().then( (chainId) => {
-    console.log( `Deploying ${contractName} on network ${getNetworkName()} (chainId:${chainId})  with args:` );
-    console.dir( args );
-    console.log( "TODO" );
-    console.log( "TODO" );
-    console.log( "TODO" );
-    console.log( "TODO" );
-  });
-/*
+  const chainId = await getChainId();
+  const networkName = getNetworkName()
   const {deployer} = await getNamedAccounts();
+
+  const getParamsArgs = (chainId) => {
+    return matchingMarket_params.args[chainId];
+  };
+
+
+
+  const token_deployment = await deployments.get(token_contractName);
+  const token_address = token_deployment.address
+
+  const oracle_deployment = await deployments.get(uniswapV3Oracle_contractName);
+  const oracle_address = oracle_deployment.address;
+
+  const matchingMarket_args = getParamsArgs(chainId);
+  const isMarketSuspended = matchingMarket_args[0];
+  const dustLimit = matchingMarket_args[1];
+
+  // console.log( `Oracle address: ${oracle_address}` );
+  // console.log( `oracle_deployment.args:` );
+  // console.dir( oracle_deployment.args );
+  const oracle_args_token_address = oracle_deployment.args[1];
+
+  // constructor(IERC20 _mainTradableToken, bool _suspended, IERC20 _dustToken, uint128 _dustLimit, address _priceOracle) RestrictedSuspendableSimpleMarket(_mainTradableToken, _suspended) {
+  const args = [ token_address, isMarketSuspended, oracle_args_token_address, dustLimit, oracle_address ];
+
+  console.log( `Deploying ${contractName} on network ${networkName} (chainId:${chainId})  with args:` );
+  console.dir( args );
+
+
   // the following will only deploy "{contractName}" if the contract was never deployed or if the code changed since last deployment
+
   const deployResult = await deploy(
     contractName,
     {
@@ -38,9 +67,9 @@ module.exports = async (
       `${contractName} deployed at ${deployResult.address} using ${deployResult.receipt.gasUsed} gas`
     );
   }
-*/
+
 
 };
 
 module.exports.tags = ['MatchingMarket'];
-module.exports.dependencies = ['Oracle'];
+module.exports.dependencies = [ 'Token', 'Oracle'];
