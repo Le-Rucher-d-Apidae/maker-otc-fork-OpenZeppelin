94af633d8ce8c960b5ee6d3bedf3676d3224ff20
BenoistP <bp_github.com@gazouille.net>
2023-07-06 19:42:14 +0200

    logs

--- contracts/apidaeERC20.sol
+++ contracts/apidaeERC20.sol
@@ -12,13 +12,12 @@ import "forge-std/console2.sol";
 contract ApidaeERC20 is ERC20, ERC20Burnable, Ownable {
 
     constructor(string memory name_, string memory symbol_, uint256 _totalSupply, address _totalSupplyOwner) ERC20(name_, symbol_) {
-        console2.log( "ApidaeERC20 constructor:_mint tokens #", _totalSupply);
         uint256 mintAmount = _totalSupply * 10 ** decimals();
         address _mintSupplyTo = (_totalSupplyOwner != address(0) ? _totalSupplyOwner : msg.sender);
 
         _mint( _mintSupplyTo, mintAmount);
         // console2.log( "ApidaeERC20 constructor:_mint ", _totalSupply, "tokens (#tokens units: ", mintAmount, ") to ", _mintSupplyTo );
-        console2.log( "ApidaeERC20 constructor:_mint ()", _totalSupply, " tokens) to; ", _mintSupplyTo );
+        console2.log( "ApidaeERC20 constructor:_mint (", _totalSupply, " tokens) to; ", _mintSupplyTo );
         console2.log( "ApidaeERC20 constructor:_mint tokens units:", mintAmount );
     }
 
