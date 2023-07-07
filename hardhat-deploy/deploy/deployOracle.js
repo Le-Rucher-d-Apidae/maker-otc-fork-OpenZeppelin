74df364213ab2138db2646cf44e721abeae70e5f
BenoistP <bp_github.com@gazouille.net>
2023-07-07 17:00:29 +0200

    revert 4c414f3: use a stable token as base token for Oracle

--- hardhat-deploy/deploy/deployOracle.js
+++ hardhat-deploy/deploy/deployOracle.js
@@ -1,6 +1,5 @@
 const { getContractName, getDeployArgs, getParamsArgs } = require("../deploy-tools/deployFn")
 
-const token_params = require("../deploy-params/ApidaeToken-params")
 const oracle_params = require("../deploy-params/UniswapV3Oracle-params")
 
 const oracle_contractName = getContractName(oracle_params);
@@ -19,18 +18,9 @@ module.exports = async (
 
   const oracle_paramsArgs = getParamsArgs(oracle_params, chainId);
   const oracle_deployArgs = getDeployArgs(oracle_params, chainId);
-
-  const token_contractName = getContractName(token_params);
-  const token_deployment = await deployments.get(token_contractName);
-  const token_address = token_deployment.address
-  const mainTradableToken = token_address;
-
-  const uniswapV3Factory_addr = oracle_paramsArgs[0];
-  const matchingMarket_allParamsArgs = [ uniswapV3Factory_addr, mainTradableToken ];
-
   console.log();
   console.log( `Deploying ${oracle_contractName} on network ${networkName} (chainId:${chainId})  with args:` );
-  const oracle_argsArrayLogs = { uniswapV3FactoryAddress: matchingMarket_allParamsArgs[0], tokenAddress: matchingMarket_allParamsArgs[1] };
+  const oracle_argsArrayLogs = { uniswapV3FactoryAddress: oracle_paramsArgs[0], tokenAddress: oracle_paramsArgs[1] };
   console.dir( oracle_argsArrayLogs );
 
   console.log( `deployArgs:` );
@@ -42,7 +32,7 @@ module.exports = async (
     {
       from: deployer,
       gasLimit: 4000000,
-      args: matchingMarket_allParamsArgs,
+      args: oracle_paramsArgs,
       deployArgs: oracle_deployArgs,
     }
   );
