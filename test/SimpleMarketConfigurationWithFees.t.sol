1278cbd17a82ee742c6642d004ec9ec7fddb5db4
BenoistP <bp_github.com@gazouille.net>
2023-07-26 18:50:06 +0200

    Update SimpleMarketConfigurationWithFees.t.sol

--- test/SimpleMarketConfigurationWithFees.t.sol
+++ test/SimpleMarketConfigurationWithFees.t.sol
@@ -150,7 +150,10 @@ contract SimpleMarketConfigurationWithFees_Constructor_Test is DSTest, VmCheat {
 
         // Should fail : overflow
         // FAIL. Reason: "Arithmetic over/underflow"
+        
         vm.expectRevert();
+        // vm.expectRevert("Arithmetic over/underflow");
+
 
         SimpleMarketConfigurationWithFees simpleMarketConfigurationWithFees = new SimpleMarketConfigurationWithFees(
             uint(type(uint256).max+ONEHUNDREDPERCENT), // Max fee > 100%
@@ -174,18 +177,18 @@ contract SimpleMarketConfigurationWithFees_ZeroTests is DSTest, VmCheat {
 
     SimpleMarketConfigurationWithFees simpleMarketConfigurationWithFees;
     address someUser;
+    uint256 constant MARKETMAXFEE = 0;
 
     function setUp() public override{
         super.setUp();
         console2.log("SimpleMarketConfigurationWithFeesTests: setUp()");
-
 
         string memory someMnemonic = "test test test test test test test test test test test junk";
         uint256 somePrivateKey = vm.deriveKey(someMnemonic, 99);
         someUser = vm.addr(somePrivateKey);
 
         simpleMarketConfigurationWithFees = new SimpleMarketConfigurationWithFees(
-            0, // Max fee > 100%
+            0, // Max fee = 0%
             0, // Current fee =  0%
             someUser,
             1000, // buy fee   = 50 % (buy fee/(buy fee+sell fee))
@@ -201,20 +204,162 @@ contract SimpleMarketConfigurationWithFees_ZeroTests is DSTest, VmCheat {
         simpleMarketConfigurationWithFees.setMarketFee(1);
     }
 
-    // function testZeroFees2() public {
+    function testZeroFeesCheckedOverflow0() public {
 
-    //     simpleMarketConfigurationWithFees.setMarketFee(1);
-    //     // check fees are 0
+        // vm.expectRevert("Arithmetic over/underflow");
+        vm.expectRevert();
+        simpleMarketConfigurationWithFees.setMarketFee(type(uint256).max+1); // = 0
+
+        // check fees are 0
+        uint256 currentMarketFee = simpleMarketConfigurationWithFees.marketFee();
+        console2.log("currentMarketFee", currentMarketFee);
+
+        assertTrue(currentMarketFee == MARKETMAXFEE);
+    }
+
+    function testZeroFeesCheckedOverflow1() public {
+
+        // vm.expectRevert("Arithmetic over/underflow");
+        vm.expectRevert();
+        simpleMarketConfigurationWithFees.setMarketFee(type(uint256).max+2); // = 1
+
+        // check fees are 0
+        uint256 currentMarketFee = simpleMarketConfigurationWithFees.marketFee();
+        console2.log("currentMarketFee", currentMarketFee);
+
+        assertTrue(currentMarketFee == MARKETMAXFEE);
+    }
+
+    function testZeroFeesUncheckedOverflow0() public {
+
+        // vm.expectRevert("Arithmetic over/underflow");
+        // vm.expectRevert("Arithmetic over/underflow");
+        uint256 newMarketFee;
+        unchecked {
+            newMarketFee = type(uint256).max+1; // = 0
+        }
+        simpleMarketConfigurationWithFees.setMarketFee( newMarketFee );
         
-    // }
+        // check fees are 0
+        uint256 currentMarketFee = simpleMarketConfigurationWithFees.marketFee();
+        console2.log("currentMarketFee", currentMarketFee);
+
+        assertTrue(currentMarketFee == MARKETMAXFEE);
+    }
+
+    function testZeroFeesUncheckedOverflow1() public {
+
+        // vm.expectRevert("Arithmetic over/underflow");
+        uint256 newMarketFee;
+        unchecked {
+            newMarketFee = type(uint256).max+2; // = 1
+        }
+
+        vm.expectRevert( bytes(_MMWFLMT011) );
+        simpleMarketConfigurationWithFees.setMarketFee( newMarketFee );
+        
+        // check fees are 0
+        uint256 currentMarketFee = simpleMarketConfigurationWithFees.marketFee();
+        console2.log("currentMarketFee", currentMarketFee);
+
+        assertTrue(currentMarketFee == MARKETMAXFEE);
+        
+    }
+
 }
 
+contract SimpleMarketConfigurationWithFees_MaxTests is DSTest, VmCheat {
+
+    SimpleMarketConfigurationWithFees simpleMarketConfigurationWithFees;
+    address someUser;
+    uint256 constant MARKETMAXFEE = ONEHUNDREDPERCENT;
+    uint256 constant MARKETINITIALFEE = 0;
+
+    function setUp() public override{
+        super.setUp();
+        console2.log("SimpleMarketConfigurationWithFeesTests: setUp()");
+
+        string memory someMnemonic = "test test test test test test test test test test test junk";
+        uint256 somePrivateKey = vm.deriveKey(someMnemonic, 99);
+        someUser = vm.addr(somePrivateKey);
+
+        simpleMarketConfigurationWithFees = new SimpleMarketConfigurationWithFees(
+            MARKETMAXFEE, // Max fee = 100%
+            MARKETINITIALFEE, // Current fee =  0%
+            someUser,
+            1000, // buy fee   = 50 % (buy fee/(buy fee+sell fee))
+            1000  // sell fee  = 50 % (sell fee/(buy fee+sell fee))
+        );
+    }
+
+    function testMaxFeesPlusOne() public {
+
+        // Should fail : Market fee > Market max fee
+        // FAIL. Reason: "Market fee too high."
+        vm.expectRevert( bytes(_MMWFLMT011) );
+        simpleMarketConfigurationWithFees.setMarketFee(ONEHUNDREDPERCENT+1);
+    }
+
+    function testMaxFeesMinusOne() public {
+        uint256 previousMarketFee = simpleMarketConfigurationWithFees.marketFee();
+        uint256 newMarketFee = ONEHUNDREDPERCENT-1;
+        // Success
+        simpleMarketConfigurationWithFees.setMarketFee(newMarketFee);
+
+        // check fees
+        uint256 currentMarketFee = simpleMarketConfigurationWithFees.marketFee();
+        // console2.log("currentMarketFee", currentMarketFee);
+        assertTrue(previousMarketFee == MARKETINITIALFEE);
+        assertTrue(currentMarketFee == newMarketFee);
+    }
+    function testMaxFeesOnePercent() public {
+        uint256 previousMarketFee = simpleMarketConfigurationWithFees.marketFee();
+        uint256 newMarketFee = ONEPERCENT;
+
+        simpleMarketConfigurationWithFees.setMarketFee(newMarketFee);
+
+        // check fees
+        uint256 currentMarketFee = simpleMarketConfigurationWithFees.marketFee();
+        // console2.log("currentMarketFee", currentMarketFee);
+        assertTrue(previousMarketFee == MARKETINITIALFEE);
+        assertTrue(currentMarketFee == newMarketFee);
+    }
+
+    function testMaxFeesOneHundredPercent() public {
+        uint256 previousMarketFee = simpleMarketConfigurationWithFees.marketFee();
+        uint256 newMarketFee = ONEHUNDREDPERCENT;
+
+        simpleMarketConfigurationWithFees.setMarketFee(newMarketFee);
+
+        // check fees
+        uint256 currentMarketFee = simpleMarketConfigurationWithFees.marketFee();
+        // console2.log("currentMarketFee", currentMarketFee);
+        assertTrue(previousMarketFee == MARKETINITIALFEE);
+        assertTrue(currentMarketFee == newMarketFee);
+    }
+
+    function testFiftyPercentFees() public {
+        uint256 previousMarketFee = simpleMarketConfigurationWithFees.marketFee();
+        uint256 newMarketFee = ONEPERCENT*50;
+
+        simpleMarketConfigurationWithFees.setMarketFee(newMarketFee);
+
+        // check fees
+        uint256 currentMarketFee = simpleMarketConfigurationWithFees.marketFee();
+        // console2.log("currentMarketFee", currentMarketFee);
+        assertTrue(previousMarketFee == MARKETINITIALFEE);
+        assertTrue(currentMarketFee == newMarketFee);
+    }
+
+}
 
 contract SimpleMarketConfigurationWithFees_CheckRatios is DSTest, VmCheat {
 
     SimpleMarketConfigurationWithFees simpleMarketConfigurationWithFees;
     address someUser;
     address someUser2;
+    uint256 constant MARKETMAXFEE = ONEHUNDREDPERCENT;
+    uint256 constant MARKETINITIALFEE = 0;
     uint constant BUYFEE = 1000;
     uint constant SELLFEE = 1000;
 
@@ -229,8 +374,8 @@ contract SimpleMarketConfigurationWithFees_CheckRatios is DSTest, VmCheat {
         someUser2 = vm.addr(somePrivateKey2);
 
         simpleMarketConfigurationWithFees = new SimpleMarketConfigurationWithFees(
-            10_000, // Max fee = 1%
-            0, // Current fee =  0%
+            MARKETMAXFEE, // Max fee = 1%
+            MARKETINITIALFEE, // Current fee =  0%
             someUser,
             BUYFEE, // buy fee   = 50 % (buy fee/(buy fee+sell fee))
             SELLFEE  // sell fee  = 50 % (sell fee/(buy fee+sell fee))
@@ -238,20 +383,156 @@ contract SimpleMarketConfigurationWithFees_CheckRatios is DSTest, VmCheat {
 
     }
 
-    function testCurrentFees1() public {
+    function testCurrentFees() public {
 
         uint marketFee = simpleMarketConfigurationWithFees.marketFee();
-        assertTrue(marketFee == 0);
+        assertTrue(marketFee == MARKETINITIALFEE);
 
         uint buyFee = simpleMarketConfigurationWithFees.buyFee();
-        assertTrue(buyFee == 0);
+        assertTrue(buyFee == MARKETINITIALFEE);
 
         uint sellFee = simpleMarketConfigurationWithFees.sellFee();
-        assertTrue(sellFee == 0);
+        assertTrue(sellFee == MARKETINITIALFEE);
+    }
+
+
+    function testMaxFeesOneHundredPercent_1Buy_1Sell_Ratios() public {
+        uint256 previousMarketFee = simpleMarketConfigurationWithFees.marketFee();
+
+        uint256 previous_buyFee = simpleMarketConfigurationWithFees.calculateBuyFee(1_000_000_000_000);
+        uint256 previous_sellFee = simpleMarketConfigurationWithFees.calculateSellFee(1_000_000_000_000);
+
+        uint256 newMarketFee = ONEHUNDREDPERCENT;
+
+        simpleMarketConfigurationWithFees.setMarketFee(newMarketFee);
+
+        // check fees
+        uint256 currentMarketFee = simpleMarketConfigurationWithFees.marketFee();
+        // console2.log("currentMarketFee", currentMarketFee);
+        assertTrue(previousMarketFee == MARKETINITIALFEE);
+        assertTrue(currentMarketFee == newMarketFee);
+
+        assertTrue(previous_buyFee == 0);
+        assertTrue(previous_sellFee == 0);
+
+        uint256 current_buyFee = simpleMarketConfigurationWithFees.calculateBuyFee(2);
+        console2.log("current_buyFee", current_buyFee);
+        uint256 current_sellFee = simpleMarketConfigurationWithFees.calculateSellFee(2);
+        console2.log("current_sellFee", current_sellFee);
+
+        assertTrue(current_buyFee == 1);
+        assertTrue(current_sellFee == 1);
+    }
+
+    function testMaxFeesOneHundredPercent_2Buy_1Sell_Ratios_part1() public {
+        // Compiler error (/solidity/libsolidity/codegen/LValue.cpp:52):Stack too deep. Try
+        uint256 previousMarketFee = simpleMarketConfigurationWithFees.marketFee();
+        uint256 previous_buyFee_1_1 = simpleMarketConfigurationWithFees.calculateBuyFee(1_000_000_000_000);
+        uint256 previous_sellFee_1_1 = simpleMarketConfigurationWithFees.calculateSellFee(1_000_000_000_000);
+
+        // set buy and sell fee buy:sell ratios 2:1
+        uint8 buyFeeRatio = 2;
+        uint8 sellFeeRatio = 1;
+        uint8 buySellFeeRatio = buyFeeRatio + sellFeeRatio;
+        simpleMarketConfigurationWithFees.setMarketBuyAndSellFeeRatios(buyFeeRatio, sellFeeRatio);
+
+        uint256 previous_buyFee_2_1 = simpleMarketConfigurationWithFees.calculateBuyFee(1_000_000_000_000);
+        uint256 previous_sellFee_2_1 = simpleMarketConfigurationWithFees.calculateSellFee(1_000_000_000_000);
+
+        uint256 newMarketFee = ONEHUNDREDPERCENT;
+
+        simpleMarketConfigurationWithFees.setMarketFee(newMarketFee);
+
+        // check fees
+        uint256 currentMarketFee = simpleMarketConfigurationWithFees.marketFee();
+        // console2.log("currentMarketFee", currentMarketFee);
+        assertTrue(previousMarketFee == MARKETINITIALFEE);
+        assertTrue(currentMarketFee == newMarketFee);
+
+        assertTrue(previous_buyFee_1_1 == 0);
+        assertTrue(previous_sellFee_1_1 == 0);
+
+        assertTrue(previous_buyFee_2_1 == 0);
+        assertTrue(previous_sellFee_2_1 == 0);
+
+        uint256 testValue_1 = 9;
+
+        uint256 current_buyFee_1 = simpleMarketConfigurationWithFees.calculateBuyFee(testValue_1); // = 9 * (2 / 2+1)
+        // console2.log("current_buyFee_1", current_buyFee_1);
+        uint256 current_sellFee_1 = simpleMarketConfigurationWithFees.calculateSellFee(testValue_1); // = 9 * (1 / 2+1)
+        // console2.log("current_sellFee_1", current_sellFee_1);
+
+        assertGe(current_buyFee_1, testValue_1*buyFeeRatio/buySellFeeRatio -1);
+        assertLe(current_buyFee_1, testValue_1*buyFeeRatio/buySellFeeRatio +1);
+        assertGe(current_sellFee_1, testValue_1*sellFeeRatio/buySellFeeRatio -1);
+        assertLe(current_sellFee_1, testValue_1*sellFeeRatio/buySellFeeRatio +1);
+    }
+
+    function testMaxFeesOneHundredPercent_2Buy_1Sell_Ratios_part2() public {
+        // set buy and sell fee buy:sell ratios 2:1
+        uint8 buyFeeRatio = 2;
+        uint8 sellFeeRatio = 1;
+        uint8 buySellFeeRatio = buyFeeRatio + sellFeeRatio;
+        simpleMarketConfigurationWithFees.setMarketBuyAndSellFeeRatios(buyFeeRatio, sellFeeRatio);
+
+        uint256 newMarketFee = ONEHUNDREDPERCENT;
+
+        simpleMarketConfigurationWithFees.setMarketFee(newMarketFee);
+
+        uint256 testValue_2 = 10;
+
+        uint256 current_buyFee_2 = simpleMarketConfigurationWithFees.calculateBuyFee(testValue_2); // = 10 * (2 / 2+1)
+        // console2.log("current_buyFee_2", current_buyFee_2);
+        uint256 current_sellFee2 = simpleMarketConfigurationWithFees.calculateSellFee(testValue_2); // = 10 * (1 / 2+1)
+        // console2.log("current_sellFee2", current_sellFee2);
+
+        assertGe(current_buyFee_2, testValue_2*buyFeeRatio/buySellFeeRatio -1);
+        assertLe(current_buyFee_2, testValue_2*buyFeeRatio/buySellFeeRatio +1);
+        assertGe(current_sellFee2, testValue_2*sellFeeRatio/buySellFeeRatio -1);
+        assertLe(current_sellFee2, testValue_2*sellFeeRatio/buySellFeeRatio +1);
+
+        uint256 testValue_3 = 1_000;
+
+        uint256 current_buyFee_3 = simpleMarketConfigurationWithFees.calculateBuyFee(testValue_3); // = 1000 * (2 / 2+1)
+        console2.log("current_buyFee_3", current_buyFee_3);
+        uint256 current_sellFee_3 = simpleMarketConfigurationWithFees.calculateSellFee(testValue_3); // = 1000 * (1 / 2+1)
+        console2.log("current_sellFee_3", current_sellFee_3);
+
+        assertGe(current_buyFee_3, testValue_3*buyFeeRatio/buySellFeeRatio -1);
+        assertLe(current_buyFee_3, testValue_3*buyFeeRatio/buySellFeeRatio +1);
+        assertGe(current_sellFee_3, testValue_3*sellFeeRatio/buySellFeeRatio -1);
+        assertLe(current_sellFee_3, testValue_3*sellFeeRatio/buySellFeeRatio +1);
+    }
+
+    function testFeesFiftyPercent_1Buy_1Sell_Ratios() public {
+        uint256 previousMarketFee = simpleMarketConfigurationWithFees.marketFee();
+
+        uint256 previous_buyFee = simpleMarketConfigurationWithFees.calculateBuyFee(1_000_000_000_000);
+        uint256 previous_sellFee = simpleMarketConfigurationWithFees.calculateSellFee(1_000_000_000_000);
+
+        uint256 newMarketFee = ONEPERCENT*50;
+
+        simpleMarketConfigurationWithFees.setMarketFee(newMarketFee);
+
+        // check fees
+        uint256 currentMarketFee = simpleMarketConfigurationWithFees.marketFee();
+        // console2.log("currentMarketFee", currentMarketFee);
+        assertTrue(previousMarketFee == MARKETINITIALFEE);
+        assertTrue(currentMarketFee == newMarketFee);
+
+        assertTrue(previous_buyFee == 0);
+        assertTrue(previous_sellFee == 0);
+
+        uint256 current_buyFee = simpleMarketConfigurationWithFees.calculateBuyFee(4);
+        console2.log("current_buyFee", current_buyFee);
+        uint256 current_sellFee = simpleMarketConfigurationWithFees.calculateSellFee(4);
+        console2.log("current_sellFee", current_sellFee);
+
+        assertTrue(current_buyFee == 1);
+        assertTrue(current_sellFee == 1);
     }
 
 /*
-
     function testUpdateCurrentFees() public {
 
         simpleMarketConfigurationWithFees.setMarketFee(10_000); // 1%
@@ -269,7 +550,6 @@ contract SimpleMarketConfigurationWithFees_CheckRatios is DSTest, VmCheat {
         assertTrue(sellFee == 10_000 * SELLFEE / (BUYFEE + SELLFEE) );
     }
 */
-
 
 /*
 
