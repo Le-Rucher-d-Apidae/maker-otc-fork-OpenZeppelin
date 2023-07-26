6209c27b7ccdc9d4c86e00f1560dbe29c9e28ae0
BenoistP <bp_github.com@gazouille.net>
2023-07-26 18:49:37 +0200

    ONEPERCENT

--- contracts/MarketsConstantsFees.sol
+++ contracts/MarketsConstantsFees.sol
@@ -4,4 +4,6 @@
 // pragma solidity ^0.8.20;
 pragma solidity ^0.8.18; // latest HH supported version
 
+// One percent = 10_000
+uint256 constant ONEPERCENT = 10_000;
 uint256 constant ONEHUNDREDPERCENT  = 1_000_000;
