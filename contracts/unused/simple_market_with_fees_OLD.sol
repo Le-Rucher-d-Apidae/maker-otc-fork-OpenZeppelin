bc2affd123356ca03d9d8749668d2e222f3e78ed
BenoistP <bp_github.com@gazouille.net>
2023-08-21 14:58:13 +0200

    mode simple_market_with_fees_OLD.sol to unused/

--- contracts/unused/simple_market_with_fees_OLD.sol
+++ contracts/unused/simple_market_with_fees_OLD.sol
@@ -24,7 +24,7 @@ import "forge-std/console2.sol";
 // import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 // import "@openzeppelin/contracts/access/Ownable.sol";
 // import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
-import "./Simple_Market.sol";
+import "../Simple_Market.sol";
 
 contract SimpleMarketWithFeesEvents {
 
